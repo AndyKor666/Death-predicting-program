2c7d9f3051cee39ccbd1aadbb6f8f14150018173
AndyKor666 <andrey.korotkov1304@gmail.com>
2025-10-16 16:43:07 +0300

    Slow_printer function

--- Death predicting program/Death_predicting_program.py
+++ Death predicting program/Death_predicting_program.py
@@ -1,3 +1,11 @@
 import random
 import time
 from datetime import datetime, date, timedelta
+
+def slow_print(text, delay=0.03):
+    for line in text.split('\n'):
+        for char in line:
+            print(char, end='', flush=True)
+            time.sleep(delay)
+        print()
+        time.sleep(delay*10)
