2e90479c91e498b1dec4889882c563d05dc1bcd3
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-10-16 15:24:21 +0300

    File write console
    The part that writes data to the file

--- gui.py
+++ gui.py
@@ -1,5 +1,12 @@
-return ">> You will die today . . ."
-return ">> You will die tomorrow . . ."
-return ">> You will die the day after tomorrow . . ."
-return ">> ERR . . . Unexpected error in prediction.\n------------------------------------------"
-return f">> Your death date: {death.strftime('%m.%d.%Y')}"   
+
+
+f.write("============== FUTURE DEATH ==============\n")        
+f.write("------------------------------------------\n")        
+f.write(f">> Name: {name}\n")        
+f.write(f">> Birth: {birth_str}\n")        
+f.write("------------------------------------------\n")        
+f.write(result + "\n")        
+f.write("------------------------------------------\n")        
+f.write(f">> Record created at {datetime.now().strftime('%H:%M:%S %d.%m.%Y')}\n")        
+f.write("------------------------------------------\n")        
+f.write("==========================================\n")   
